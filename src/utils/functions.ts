ed568be2fbaed27672bbeeb2cf3f1286b61e8596
Matheus <matheusrcrv07@gmail.com>
2023-01-23 20:13:30 -0300

    fix: ATOB method obsolete, apply Buffer method

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,4 +1,4 @@
 export const decodeToken = (token: string) => {
-  const { id } = JSON.parse(atob(token.split(".")[1]));
+  const { id } = JSON.parse(Buffer.from(token.split(".")[1], 'base64').toString());
   return id;
 };
